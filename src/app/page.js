02f1cd3b5b642d616b0ff75a0e3d32cad2358fba
Alexsander Gutierrez <alexsandergutierreez@gmail.com>
2023-09-29 18:04:05 -0300

    changes layout

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import Weather from './weather';
 
 async function getData(q) {
   try {
@@ -6,10 +7,9 @@ async function getData(q) {
       process.env.API_KEY
     }&q=${encodeURI(q)}&aqi=no&lang=pt`;
 
-    const request = await fetch(url, { next: { revalidate: 100 } });
+    const request = await fetch(url, { next: { revalidate: 1 } });
 
     const json = await request.json();
-
     return json;
   } catch (error) {
     return null;
@@ -18,13 +18,14 @@ async function getData(q) {
 
 export default async function Home({ searchParams }) {
   const data = searchParams.q ? await getData(searchParams.q) : null;
+
   return (
     <div className="items-center justify-between flex flex-col p-4">
       <main className="flex flex-col gap-y-4 w-full">
         <form className="inline-flex justify-center gap-x-4">
           <input
             className="rounded-full p-4 outline-none shadow-md w-full sm:w-1/2 outline-offset-0 hover:outline-cyan-500 focus:outline-cyan-400"
-            placeholder="Digite o nome da cidade ou região..."
+            placeholder="Buscar"
             type="text"
             name="q"
             autoComplete="off"
@@ -45,57 +46,14 @@ export default async function Home({ searchParams }) {
             </svg>
           </button>
         </form>
-        {data && (
-          <div className="text-white flex flex-col gap-y-4 items-center">
-            <Image
-              src={`https:${data.current.condition.icon}`}
-              width={64}
-              height={64}
-              alt="icon"
-            />
-            <h2 className="text-4xl font-bold">{data.current.temp_c}°</h2>
-            <h3 className="text-2xl font-medium text-center">
-              {data.location.name}, {data.location.region}
-            </h3>
-            <h4 className="text-xl font-semibold text-center">
-              {data.current.condition.text}
-            </h4>
-
-            <div className="flex gap-x-4 justify-between text-lg font-semibold">
-              <div className="items-center gap-y-2 flex flex-col">
-                <div className="flex gap-x-2 items-center">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
-                    fill="currentColor"
-                    viewBox="0 0 16 16"
-                  >
-                    <path d="M.036 3.314a.5.5 0 0 1 .65-.278l1.757.703a1.5 1.5 0 0 0 1.114 0l1.014-.406a2.5 2.5 0 0 1 1.857 0l1.015.406a1.5 1.5 0 0 0 1.114 0l1.014-.406a2.5 2.5 0 0 1 1.857 0l1.015.406a1.5 1.5 0 0 0 1.114 0l1.757-.703a.5.5 0 1 1 .372.928l-1.758.703a2.5 2.5 0 0 1-1.857 0l-1.014-.406a1.5 1.5 0 0 0-1.114 0l-1.015.406a2.5 2.5 0 0 1-1.857 0l-1.014-.406a1.5 1.5 0 0 0-1.114 0l-1.015.406a2.5 2.5 0 0 1-1.857 0L.314 3.964a.5.5 0 0 1-.278-.65zm0 3a.5.5 0 0 1 .65-.278l1.757.703a1.5 1.5 0 0 0 1.114 0l1.014-.406a2.5 2.5 0 0 1 1.857 0l1.015.406a1.5 1.5 0 0 0 1.114 0l1.014-.406a2.5 2.5 0 0 1 1.857 0l1.015.406a1.5 1.5 0 0 0 1.114 0l1.757-.703a.5.5 0 1 1 .372.928l-1.758.703a2.5 2.5 0 0 1-1.857 0l-1.014-.406a1.5 1.5 0 0 0-1.114 0l-1.015.406a2.5 2.5 0 0 1-1.857 0l-1.014-.406a1.5 1.5 0 0 0-1.114 0l-1.015.406a2.5 2.5 0 0 1-1.857 0L.314 6.964a.5.5 0 0 1-.278-.65zm0 3a.5.5 0 0 1 .65-.278l1.757.703a1.5 1.5 0 0 0 1.114 0l1.014-.406a2.5 2.5 0 0 1 1.857 0l1.015.406a1.5 1.5 0 0 0 1.114 0l1.014-.406a2.5 2.5 0 0 1 1.857 0l1.015.406a1.5 1.5 0 0 0 1.114 0l1.757-.703a.5.5 0 1 1 .372.928l-1.758.703a2.5 2.5 0 0 1-1.857 0l-1.014-.406a1.5 1.5 0 0 0-1.114 0l-1.015.406a2.5 2.5 0 0 1-1.857 0l-1.014-.406a1.5 1.5 0 0 0-1.114 0l-1.015.406a2.5 2.5 0 0 1-1.857 0L.314 9.964a.5.5 0 0 1-.278-.65zm0 3a.5.5 0 0 1 .65-.278l1.757.703a1.5 1.5 0 0 0 1.114 0l1.014-.406a2.5 2.5 0 0 1 1.857 0l1.015.406a1.5 1.5 0 0 0 1.114 0l1.014-.406a2.5 2.5 0 0 1 1.857 0l1.015.406a1.5 1.5 0 0 0 1.114 0l1.757-.703a.5.5 0 1 1 .372.928l-1.758.703a2.5 2.5 0 0 1-1.857 0l-1.014-.406a1.5 1.5 0 0 0-1.114 0l-1.015.406a2.5 2.5 0 0 1-1.857 0l-1.014-.406a1.5 1.5 0 0 0-1.114 0l-1.015.406a2.5 2.5 0 0 1-1.857 0l-1.757-.703a.5.5 0 0 1-.278-.65z" />
-                  </svg>
-                  <p>{data.current.humidity} %</p>
-                </div>
-                <p className="text-center">Umidade do ar</p>
-              </div>
-
-              <div className="items-center gap-y-2 flex flex-col">
-                <div className="flex gap-x-2 items-center">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
-                    fill="currentColor"
-                    viewBox="0 0 16 16"
-                  >
-                    <path d="M12.5 2A2.5 2.5 0 0 0 10 4.5a.5.5 0 0 1-1 0A3.5 3.5 0 1 1 12.5 8H.5a.5.5 0 0 1 0-1h12a2.5 2.5 0 0 0 0-5zm-7 1a1 1 0 0 0-1 1 .5.5 0 0 1-1 0 2 2 0 1 1 2 2h-5a.5.5 0 0 1 0-1h5a1 1 0 0 0 0-2zM0 9.5A.5.5 0 0 1 .5 9h10.042a3 3 0 1 1-3 3 .5.5 0 0 1 1 0 2 2 0 1 0 2-2H.5a.5.5 0 0 1-.5-.5z" />
-                  </svg>
-                  <p>{data.current.wind_kph} km/h</p>
-                </div>
-                <p className="text-center">Velocidade do vento</p>
-              </div>
-            </div>
-          </div>
-        )}
+        {searchParams.q &&
+          (data != null && !data?.error ? (
+            <Weather data={data} />
+          ) : (
+            <p className="text-center text-red-500 font-bold text-xl">
+              Nenhum resultado encontrado
+            </p>
+          ))}
       </main>
     </div>
   );
---
+++ src/app/weather.js
@@ -0,0 +1,55 @@
+import Image from 'next/image';
+
+export default function Weather({ data }) {
+  return (
+    <div className="text-white flex flex-col gap-y-4 items-center">
+      <Image
+        src={`https:${data.current.condition.icon}`}
+        width={64}
+        height={64}
+        alt="icon"
+      />
+      <h2 className="text-4xl font-bold">{data.current.temp_c}°</h2>
+      <h3 className="text-2xl font-medium text-center">
+        {data.location.name}, {data.location.region}
+      </h3>
+      <h4 className="text-xl font-semibold text-center">
+        {data.current.condition.text}
+      </h4>
+
+      <div className="flex gap-x-4 justify-between text-lg font-semibold">
+        <div className="items-center gap-y-2 flex flex-col">
+          <div className="flex gap-x-2 items-center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              fill="currentColor"
+              viewBox="0 0 16 16"
+            >
+              <path d="M.036 3.314a.5.5 0 0 1 .65-.278l1.757.703a1.5 1.5 0 0 0 1.114 0l1.014-.406a2.5 2.5 0 0 1 1.857 0l1.015.406a1.5 1.5 0 0 0 1.114 0l1.014-.406a2.5 2.5 0 0 1 1.857 0l1.015.406a1.5 1.5 0 0 0 1.114 0l1.757-.703a.5.5 0 1 1 .372.928l-1.758.703a2.5 2.5 0 0 1-1.857 0l-1.014-.406a1.5 1.5 0 0 0-1.114 0l-1.015.406a2.5 2.5 0 0 1-1.857 0l-1.014-.406a1.5 1.5 0 0 0-1.114 0l-1.015.406a2.5 2.5 0 0 1-1.857 0L.314 3.964a.5.5 0 0 1-.278-.65zm0 3a.5.5 0 0 1 .65-.278l1.757.703a1.5 1.5 0 0 0 1.114 0l1.014-.406a2.5 2.5 0 0 1 1.857 0l1.015.406a1.5 1.5 0 0 0 1.114 0l1.014-.406a2.5 2.5 0 0 1 1.857 0l1.015.406a1.5 1.5 0 0 0 1.114 0l1.757-.703a.5.5 0 1 1 .372.928l-1.758.703a2.5 2.5 0 0 1-1.857 0l-1.014-.406a1.5 1.5 0 0 0-1.114 0l-1.015.406a2.5 2.5 0 0 1-1.857 0l-1.014-.406a1.5 1.5 0 0 0-1.114 0l-1.015.406a2.5 2.5 0 0 1-1.857 0L.314 6.964a.5.5 0 0 1-.278-.65zm0 3a.5.5 0 0 1 .65-.278l1.757.703a1.5 1.5 0 0 0 1.114 0l1.014-.406a2.5 2.5 0 0 1 1.857 0l1.015.406a1.5 1.5 0 0 0 1.114 0l1.014-.406a2.5 2.5 0 0 1 1.857 0l1.015.406a1.5 1.5 0 0 0 1.114 0l1.757-.703a.5.5 0 1 1 .372.928l-1.758.703a2.5 2.5 0 0 1-1.857 0l-1.014-.406a1.5 1.5 0 0 0-1.114 0l-1.015.406a2.5 2.5 0 0 1-1.857 0l-1.014-.406a1.5 1.5 0 0 0-1.114 0l-1.015.406a2.5 2.5 0 0 1-1.857 0L.314 9.964a.5.5 0 0 1-.278-.65zm0 3a.5.5 0 0 1 .65-.278l1.757.703a1.5 1.5 0 0 0 1.114 0l1.014-.406a2.5 2.5 0 0 1 1.857 0l1.015.406a1.5 1.5 0 0 0 1.114 0l1.014-.406a2.5 2.5 0 0 1 1.857 0l1.015.406a1.5 1.5 0 0 0 1.114 0l1.757-.703a.5.5 0 1 1 .372.928l-1.758.703a2.5 2.5 0 0 1-1.857 0l-1.014-.406a1.5 1.5 0 0 0-1.114 0l-1.015.406a2.5 2.5 0 0 1-1.857 0l-1.014-.406a1.5 1.5 0 0 0-1.114 0l-1.015.406a2.5 2.5 0 0 1-1.857 0l-1.757-.703a.5.5 0 0 1-.278-.65z" />
+            </svg>
+            <p>{data.current.humidity} %</p>
+          </div>
+          <p className="text-center">Umidade do ar</p>
+        </div>
+
+        <div className="items-center gap-y-2 flex flex-col">
+          <div className="flex gap-x-2 items-center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              fill="currentColor"
+              viewBox="0 0 16 16"
+            >
+              <path d="M12.5 2A2.5 2.5 0 0 0 10 4.5a.5.5 0 0 1-1 0A3.5 3.5 0 1 1 12.5 8H.5a.5.5 0 0 1 0-1h12a2.5 2.5 0 0 0 0-5zm-7 1a1 1 0 0 0-1 1 .5.5 0 0 1-1 0 2 2 0 1 1 2 2h-5a.5.5 0 0 1 0-1h5a1 1 0 0 0 0-2zM0 9.5A.5.5 0 0 1 .5 9h10.042a3 3 0 1 1-3 3 .5.5 0 0 1 1 0 2 2 0 1 0 2-2H.5a.5.5 0 0 1-.5-.5z" />
+            </svg>
+            <p>{data.current.wind_kph} km/h</p>
+          </div>
+          <p className="text-center">Velocidade do vento</p>
+        </div>
+      </div>
+    </div>
+  );
+}
